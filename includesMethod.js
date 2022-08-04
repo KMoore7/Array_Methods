b9dc513cb229463802d8ebf8002e523e2840311d
KMoore7 <carla.moore3@icloud.com>
2022-08-03 20:03:54 -0400

    This is how you use the includes method to do a check to see if an element is in your array

---
+++ includesMethod.js
@@ -0,0 +1,8 @@
+//This method does not use a function. It simply checks to see if an array has an element without doing an extensive check.
+//Returns true or false
+
+const items = [1,2,3,4,5]
+
+const includesTwo = items.includes(2)
+
+console.log(includesTwo)
