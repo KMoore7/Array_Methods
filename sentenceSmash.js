3b7089e568195c4ecbb0bd93c79caa61d3d9f1e6
KMoore7 <carla.moore3@icloud.com>
2022-08-12 07:54:56 -0400

    This is a codewars to smash the words in an array into a sentence with a space between each word

---
+++ sentenceSmash.js
@@ -0,0 +1,9 @@
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+function smash (words) { //use the join method to put words from an array into a sentence. use the space parameter to get spaces between each word.
+    return words.join(' ');
+ };
