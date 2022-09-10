d8d5be61598d5cd2c1d74d63ad594988d36e83ea
KMoore7 <carla.moore3@icloud.com>
2022-09-10 09:00:49 -0400

    This is a codewards on how to use the .include method

---
+++ codewarsIncludeMethod.js
@@ -0,0 +1,7 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+
+
+function include(arr, item){ //create a function named inlcude with 2 parameters: arr, item
+    return arr.includes(item); //use the include function to reutrn true or false if parameter arr includes the item item
+   }
